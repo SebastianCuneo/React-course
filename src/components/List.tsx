33f7840067791b0c1080e031a9ee480a8fa79806
unknown <sebastian.cuneo.s1@gmail.com>
2025-04-23 18:35:10 -0300

    Props, childens, etc

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,21 @@
+import { MouseEvent } from "react";
+
 type Props = {
   data: string[];
 };
 
 function List({ data }: Props) {
+  const handleClick = (e: string) => {
+    console.log(e);
+  };
   return (
     <ul className="list-group">
       {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">
+        <li
+          onClick={() => handleClick(elemento)}
+          key={elemento}
+          className="list-group-item"
+        >
           {elemento}
         </li>
       ))}
